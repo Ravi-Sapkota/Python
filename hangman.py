05787e77d78e6ea410f8e0cf36c9372288cb5b55
Ravi Sapkota <rudishs25@gmail.com>
2024-01-24 22:01:32 +0545

    validation of repeating inputs

--- hangman.py
+++ hangman.py
@@ -49,25 +49,31 @@ def hangman():
     while attemps > 0:
         ch = input("\nChoose a character: ").lower()
         if len(ch) == 1 and ch.isalpha():
-            if ch in key:
+            if ch in guessed:
+                print("You already tried it.")
+            elif ch in key:
                 print("Your guess is correct.")
                 guessed += ch
             else:
                 print("Your guess is wrong.")
+                guessed += ch
                 attemps = attemps - 1
         else:
             print("Wrong input. Please try a single character.")
         print(f"Attemps left = {attemps}")
-        print(display(key, guessed))
-        if len(set(key)) == len(set(guessed)):
-            return True
+        print(guessed)
+        temp = display(key, guessed)
+        print(temp)
+        if not "*" in temp:
+            return attemps
     if attemps == 0:
         print("Your word was '{}'".format(key))
+        return attemps
 
 
 game = hangman()
 print("\n" * 3)
-if game == True:
-    print("You Won")
-else:
+if game == 0:
     print("You lost")
+else:
+    print("You won")
