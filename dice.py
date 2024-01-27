1e63e2787b0dfec893ef369af41ba654122cd77f
Ravi Sapkota <rudishs25@gmail.com>
2024-01-27 18:48:41 +0545

    conflict on global variable solved

--- dice.py
+++ dice.py
@@ -1,11 +1,12 @@
 import random
 
-board = [1, 2, 3, 4, 5, 6]
+board = [1, 1, 2, 2, 3, 4, 5, 6]
 
 balance = 0
 
 
 def financial_eligibility():
+    global balance  # Declare balance as a global variable
     if balance > -100:
         darts()
     else:
@@ -13,28 +14,31 @@ def financial_eligibility():
 
 
 def betting_choice():
-    choice = int(input("1. Sums less than 7\n2. Sums 7\n3. Sums more than 7"))
+    choice = int(
+        input("1. Sums less than 7\n2. Sums 7\n3. Sums more than 7\nYour choice: ")
+    )
     if choice > 3 or choice < 1:
-        betting_choice()
+        return betting_choice()
     return choice
 
 
-def win_condition(bet, sum):
-    if bet == 1 and sum < 7:
+def win_condition(bet, total_sum):
+    global balance  # Declare balance as a global variable
+    if bet == 1 and total_sum < 7:
         print("Congratulations you won.")
         balance = balance + 20
-        print("Your balance is now {}".format(balance))
-    elif bet == 2 and sum == 7:
+    elif bet == 2 and total_sum == 7:
         print("Congratulations you won.")
         balance = balance + 40
-    elif bet == 3 and sum > 7:
+    elif bet == 3 and total_sum > 7:
         print("Congratulations you won.")
         balance = balance + 20
     else:
         print("Sorry you've lost.")
+        balance = balance - 20
     print("Your new balance is {}".format(balance))
-    playagain = input("Do you want to play again?[y/n]").lower()
-    if playagain == "y":
+    play_again = input("Do you want to play again? [y/n]\n").lower()
+    if play_again == "y":
         financial_eligibility()
 
 
@@ -46,15 +50,15 @@ def darts():
     print("Your dart 1 hit the number {}".format(d1))
     print("Your dart 2 hit the number {}".format(d2))
     print("Your dart 3 hit the number {}".format(d3))
-    sum = d1 + d2 + d3
-    print("Your sum is {}".format(sum))
-    win_condition(sum, bet)
+    total_sum = d1 + d2 + d3
+    print("Your sum is {}".format(total_sum))
+    win_condition(bet, total_sum)
 
 
 print("You get 3 darts to hit on a board with numbers: 1-6.")
-print("You need to bet on wheather you'll sum up to (less than 7, 7 or more than 7)")
-print("If you bet on 7 and win then you'll win twice the money of your entry fee.")
-print("If you bet on others and win then you'll win the same money of your entry fee.")
+print("You need to bet on whether you'll sum up to (less than 7, 7, or more than 7)")
+print("If you bet on 7 and win, then you'll win twice the money of your entry fee.")
+print("If you bet on others and win, then you'll win the same money as your entry fee.")
 print("If you don't make up to your bet, you'll lose your money.\n")
 print("Entry fee = 20:")
 
