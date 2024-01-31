f46aa6ebe09d91af9b4de07da8f2e61d13bef454
Ravi Sapkota <rudishs25@gmail.com>
2024-01-31 09:28:19 +0545

    new from scratch, input validation done

--- tiktaktoe.py
+++ tiktaktoe.py
@@ -1,32 +1,39 @@
-def win_condition():
-    return 0
+player = ["X", "O"]
+chart = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+used = []
 
 
-def display_figure():
+def get_position():
+    index = int(input("Choose the position [1-9]: "))
+    while index > 9 or index < 1:
+        print(f"Your position {index} is invalid.")
+        return get_position()
+    while index in used:
+        print(f"You have already value({index}).")
+        return get_position()
+    used.append(index)
+    print(f"You have used: {used}")
+    return index
+
+
+def display():
     i = 0
-    for value in value_dict.values():
+    for ch in chart:
         i = i + 1
         if i % 3:
-            print(value, end=" | ")
+            print(ch, end=" | ")
         else:
-            print(value)
+            print(ch)
 
 
-value_dict = {1: " ", 2: " ", 3: " ", 4: " ", 5: " ", 6: " ", 7: " ", 8: " ", 9: " "}
+def tiktaktoe():
+    i = 1
+    while i < 10:
+        print(f"It is {player[i%2]}'s turn:")
+        position = get_position()
+        chart[position - 1] = player[i % 2]
+        display()
+        i = i + 1
 
-turn = ["X", "O"]
-turn_selector = 1
-print("Choose position as: ")
-for key in value_dict:
-    if key % 3:
-        print(key, end=" ")
-    else:
-        print(key)
 
-while not win_condition() and turn_selector < 10:
-    mark = turn[turn_selector % 2]
-    print("It is {}'s turn.".format(mark))
-    position = int(input("Choose the index of position as above: "))
-    value_dict[position] = mark
-    turn_selector = turn_selector + 1
-    display_figure()
+tiktaktoe()
